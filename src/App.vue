15be18461a0f59ff15917c3421339bf7634a3125
haejin <luckyhaejin1126@gmail.com>
2023-09-27 02:45:12 +0900

    change repository name & use function

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,17 @@
 <template>
-  <div class="name">{{name}}</div>
+  <div class="name">{{ greet }}</div>
 </template>
  
 <script>
 export default {
   setup() {
     const name = 'Lucky Haejin'
+    const greeting = (name) => {
+      return 'Hello, ' + name;
+    };
+    const greet = greeting(name);
     return {
-      name
+      greet
     };
   },
 }
