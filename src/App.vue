aa7e151a831f9fdbbbb780378a40bc02e2b2d0de
haejin <luckyhaejin1126@gmail.com>
2023-09-27 23:34:56 +0900

    event

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,24 @@
 <template>
-  <div class="name">{{ greet }}</div>
+  <div class="name">{{ name }}</div>
+  <button 
+  class="btn btn-primary"
+  v-on:click="updateName"
+  >
+  Click
+  </button>
 </template>
  
 <script>
 export default {
   setup() {
-    const name = 'Lucky Haejin'
-    const greeting = (name) => {
-      return 'Hello, ' + name;
-    };
-    const greet = greeting(name);
+    let name = 'Lucky Haejin'
+    const updateName = () => { 
+      name = 'Happy Haejin';//반응형이 아니라서 console출력결과만 바뀌고 화면은 바뀌지 않음
+      console.log(name)
+    }
     return {
-      greet
+      name,
+      updateName
     };
   },
 }
