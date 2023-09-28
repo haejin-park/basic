8be6af5cbb82383dd1779d0b2985ff79e5975eb1
haejin <luckyhaejin1126@gmail.com>
2023-09-28 17:52:52 +0900

    delete todo

--- src/App.vue
+++ src/App.vue
@@ -26,13 +26,16 @@
         This field cannot be empty
       </div>
     </form>
+    <div v-if="!todos.length">
+      추가된 Todo가 없습니다.
+    </div>
     <div 
-      v-for="todo in todos"
+      v-for="(todo, index) in todos"
       :key="todo.id"
       class="card mt-2"
     >
-      <div class="card-body p-2">
-        <div class="form-check">
+      <div class="card-body p-2 d-flex align-items-center">
+        <div class="form-check flex-grow-1">
           <input
             class="form-check-input"
             type="checkbox"
@@ -45,6 +48,14 @@
             {{ todo.subject }}
           </label>
         </div>
+        <div>
+          <button 
+            class="btn btn-danger btn-sm"
+            @click="deleteTodo(index)"
+          >
+            Delete
+          </button>
+        </div>
       </div>
     </div>
   </div>
@@ -55,6 +66,10 @@
 import {ref} from 'vue';
 export default {
   setup() {
+    const deleteTodo = (index) => {
+      console.log('delete todo');
+      todos.value.splice(index,1 );
+    }
     const hasError = ref(false);
     const todo = ref('');
     const todos = ref([]);
@@ -74,6 +89,7 @@ export default {
     }
 
     return {
+      deleteTodo,
       hasError,
       todo,
       todos,
