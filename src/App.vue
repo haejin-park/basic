b9e5f8e7796f1afe74d9a2e61b3e361b0585b33a
haejin <luckyhaejin1126@gmail.com>
2023-10-01 12:08:38 +0900

    props

--- src/App.vue
+++ src/App.vue
@@ -7,35 +7,11 @@
     <div v-if="!todos.length">
       추가된 Todo가 없습니다.
     </div>
-    <div 
-      v-for="(todo, index) in todos"
-      :key="todo.id"
-      class="card mt-2"
-    >
-      <div class="card-body p-2 d-flex align-items-center">
-        <div class="form-check flex-grow-1">
-          <input
-            class="form-check-input"
-            type="checkbox"
-            v-model="todo.completed"
-          >
-          <label 
-            class="form-check-label"
-            :class="{todo:todo.completed}"  
-          >
-            {{ todo.subject }}
-          </label>
-        </div>
-        <div>
-          <button 
-            class="btn btn-danger btn-sm"
-            @click="deleteTodo(index)"
-          >
-            Delete
-          </button>
-        </div>
-      </div>
-    </div>
+    <TodoList
+      :todos="todos"
+      @toggle-todo="toggleTodo"
+      @delete-todo="deleteTodo"
+    />
   </div>
 
 </template>
@@ -43,9 +19,11 @@
 <script>
 import {ref} from 'vue';
 import TodoSimpleForm from './components/TodoSimpleForm.vue';
+import TodoList from './components/TodoList.vue';
 export default {
   components: {
-    TodoSimpleForm
+    TodoSimpleForm,
+    TodoList
   },
   setup() {
     const todos = ref([]);
@@ -53,14 +31,21 @@ export default {
       console.log(todo);
       todos.value.push(todo);
     }
+    const toggleTodo = (index) => {//체크박스 클릭 전 후 completed바뀌는지 확인
+      console.log(todos.value[index].completed);
+      todos.value[index].completed = !todos.value[index].completed
+      console.log(todos.value[index].completed);
+    }
     const deleteTodo = (index) => {
       todos.value.splice(index,1);
     }
 
+
     return {
-      deleteTodo,
       todos,
-      addTodo
+      addTodo,
+      toggleTodo,
+      deleteTodo
     };
   },
 }
