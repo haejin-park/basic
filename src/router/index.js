fe0f6c8ef365c48e8dd711ddf3fc1ccdd40dcf7d
haejin <luckyhaejin1126@gmail.com>
2023-10-02 21:29:38 +0900

    add route /todo/_id

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import {createRouter, createWebHistory} from 'vue-router';
 import Home from '../pages/index.vue';
 import Todos from '../pages/todos/index.vue';
+import Todo from '../pages/todos/_id.vue';
+
 const router = createRouter({
     history: createWebHistory(),
     routes: [
@@ -13,6 +15,11 @@ const router = createRouter({
             path: '/todos',
             name: '/Todos',
             component: Todos
+        },
+        {
+            path: '/todos/:id',
+            name: '/Todo',
+            component: Todo
         }
     ]
 });
