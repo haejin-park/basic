883c7d979b7edc837269f328e5f32f64db0b3f79
haejin <luckyhaejin1126@gmail.com>
2023-10-03 22:36:19 +0900

    make todo page component-3(onSave)

--- src/pages/todos/_id.vue
+++ src/pages/todos/_id.vue
@@ -3,7 +3,10 @@
     <div v-if="loading">
         Loading...
     </div>
-    <form v-else>
+    <form 
+    v-else
+    @submit.prevent="onSave"
+    >
         <div class="row">
             <div class="col-6">
                 <div class="form-group">
@@ -52,8 +55,9 @@ export default {
         const router = useRouter();
         const todo = ref(null);
         const loading = ref(true);
+        const todoId = route.params.id
         const getTodo = async () => {
-            const res = await axios.get('http://localhost:3000/todos/' + route.params.id);           
+            const res = await axios.get(`http://localhost:3000/todos/${todoId}`);           
             todo.value = res.data;
             loading.value = false;
         }
@@ -65,12 +69,20 @@ export default {
                 name:'Todos'
             })
         };
+        const onSave = async () => {
+            const res = await axios.put(`http://localhost:3000/todos/${todoId}`, {
+                subject: todo.value.subject,
+                completed:todo.value.completed
+            });
+            console.log(res);
+        }
         getTodo();
         return {
             todo,
             loading,
             toggleTodoStatus,
-            moveTodoListPage
+            moveTodoListPage,
+            onSave
         }
     }
 }
