bdc3bd66672691fe40ee0bcd7ec81f2cef3e075d
haejin <luckyhaejin1126@gmail.com>
2023-10-07 20:22:53 +0900

    vuex mutations/actions/getters

--- src/composables/toast.js
+++ src/composables/toast.js
@@ -1,26 +1,13 @@
-import {computed,onUnmounted} from 'vue'
+import {computed} from 'vue'
 import {useStore} from 'vuex'
 export const useToast = () => {
     const store = useStore();
-    const showToast =  computed(()=>store.state.showToast)
-    const toastMessage =  computed(()=>store.state.toastMessage)
+    const toastMessage =  computed(()=>store.getters.toastMessageWithSmile)
     const toastAlertType = computed(()=>store.state.toastAlertType)
-    const timeout =  computed(()=>store.state.timeout)
+    const showToast =  computed(()=>store.state.showToast)
     const triggerToast = (message, type= 'success') => {
-        toastMessage.value = message;
-        toastAlertType.value = type;
-        showToast.value = true;
-        timeout.value = setTimeout(() => {
-            console.log('hello');
-            toastMessage.value = '';
-            toastAlertType.value = '';
-            showToast.value = false;
-        }, 3000);
+        store.dispatch('triggerToast',{message, type})
     }
-    //다른 페이지 이동시 setTimeout멈추게해서 hello안뜨게 하고 싶을 떄 
-    onUnmounted(() => {
-        clearTimeout(timeout.value);
-    });
     return {
         showToast,
         toastMessage,
--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,37 @@
 import {createStore} from 'vuex';
 export default createStore({
     state: {
-        showToast : false,
         toastMessage : '',
         toastAlertType : '',
+        showToast : false,
         timeout : null
+    }, 
+    mutations: {
+        UPDATE_TOAST_MESSAGE(state, payload) {
+            state.toastMessage = payload;
+        },
+        UPDATE_TOAST_ALERT_TYPE(state, payload) {
+            state.toastAlertType = payload;      
+        },
+        UPDATE_TOAST_STATUS(state, payload) {
+            state.showToast = payload;
+        }
+    },
+    actions: {
+        triggerToast({commit}, payload){
+            commit('UPDATE_TOAST_MESSAGE', payload.message);
+            commit('UPDATE_TOAST_ALERT_TYPE',payload.type);
+            commit('UPDATE_TOAST_STATUS', true);
+            setTimeout(() => {
+                commit('UPDATE_TOAST_MESSAGE', '');
+                commit('UPDATE_TOAST_ALERT_TYPE','');
+                commit('UPDATE_TOAST_STATUS', false);
+            }, 5000)
+        }
+    },
+    getters: {
+        toastMessageWithSmile(state){
+            return state.toastMessage + '^_^'
+        }
     }
 }); 
