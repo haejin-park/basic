1cd1c1cedecf3ad07ea630e16b5704191f19344c
haejin <luckyhaejin1126@gmail.com>
2023-10-07 18:25:17 +0900

    axios baseURL

--- src/pages/todos/index.vue
+++ src/pages/todos/index.vue
@@ -59,7 +59,7 @@
 <script>
 import {ref, computed, watch} from 'vue';
 import TodoList from '@/components/TodoList.vue';
-import axios from 'axios';
+import axios from '@/axios';
 import Toast from '@/components/Toast.vue';
 import {useToast} from '@/composables/toast'
 import {useRouter} from 'vue-router';
@@ -85,7 +85,7 @@ export default {
       currentPage.value = page;
       try {
         const res = await axios.get(
-          `http://localhost:3000/todos?_sort=id&_order=desc&subject_like=${searchText.value}&_page=${page}&_limit=${limit}`
+          `todos?_sort=id&_order=desc&subject_like=${searchText.value}&_page=${page}&_limit=${limit}`
           );
         numberOfTodos.value = res.headers['x-total-count'];
         todos.value = res.data;
@@ -99,7 +99,7 @@ export default {
     const addTodo = async (todo) => { 
       error.value = '';
       try {
-        await axios.post('http://localhost:3000/todos',{
+        await axios.post('todos',{
         subject: todo.subject,
         completed: todo.completed
       });
@@ -116,7 +116,7 @@ export default {
       error.value = '';
       const id = todos.value[index].id;
       try{
-        await axios.patch('http://localhost:3000/todos/' + id, {
+        await axios.patch('todos/' + id, {
           completed: checked
         });
         todos.value[index].completed = checked
@@ -128,7 +128,7 @@ export default {
     const deleteTodo = async (id) => {
       error.value = '';
       try {
-        await axios.delete('http://localhost:3000/todos/' + id);
+        await axios.delete('todos/' + id);
         getTodos(currentPage.value);
       } catch(err) {
         console.log(err);
